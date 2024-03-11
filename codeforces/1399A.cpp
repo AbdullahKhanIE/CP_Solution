e3bd8be34274256dc4ac0c3e4b2f5680e7e5876f
ENO <abdullahkhan.ie.official@gmail.com>
2024-03-11 19:44:03 +0600

    Update 1399A.cpp

--- codeforces/1399A.cpp
+++ codeforces/1399A.cpp
@@ -5,35 +5,30 @@ int main() {
     int t;
     cin>>t;
     while (t--) {
-        int n, flag=0;
+        int n, flag=1;
         cin>>n;
+        int array[n];
+
         if (n==1) {
-            int array[n];
             cin>>array[0];
-            cout<<"YES"<<endl;
-            break;
         }
         else {
-            int array[n];
             for (int i = 0; i < n; i++) {
                 cin>>array[i];
             }
             sort(array, array+n);
             for (int i = 1; i < n; i++) {
-                if (array[i-1]==array[i] ||(array[i]!=array[i-1]) && (array[i]-array[i-1]==1 || array[i-1]-array[i]==1)) {
-                    flag = 1;
-                }
-                else {
-                    flag=0;
+                if (array[i-1]!=array[i] && (abs(array[i]-array[i-1])!=1)) {
+                    flag = 0;
                     break;
                 }
             }
-            if (flag) {
-                cout<<"YES"<<endl;
-            }
-            else {
-                cout<<"NO"<<endl;
-            }
+        }
+        if (flag) {
+            cout<<"YES"<<endl;
+        }
+        else {
+            cout<<"NO"<<endl;
         }
     }
     return 0;
